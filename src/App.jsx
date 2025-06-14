67453b3fc7ebaefa902964b9a6d98e1f5aa8f9c6
Rupesh Merotha <rupeshmerotha2004@gmail.com>
2025-06-14 23:16:23 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+   <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-5xl font-bold text-indigo-600">devTinder</h1>
+    </div> 
+  )
+}
+
+export default App
